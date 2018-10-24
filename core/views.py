153b0d8cd4b14d63bbaf844f152e2dfe50032f4b
Kleber <kleberm@riachuelo.com.br>
2018-10-23 22:15:53 -0300

    Implementada a tela de login

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import ContatoForm
+from .forms import ContatoForm, LoginForm
 
 # Create your views here.
 def home(request):
@@ -24,3 +24,20 @@ def contato(request):
 
     context["form"] = form
     return render(request, "contato.html", context)
+
+def entrar(request):
+    context = {}
+
+    if request.POST:
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            context["mensagem"] = "Login realizado com sucesso"
+        else:
+            context["mensagem"] = "Erro ao logar"
+    else:
+        form = LoginForm()
+        #context["mensagem"] = "Foi um GET"
+
+    context["form"] = form
+    return render(request, "entrar.html", context)
