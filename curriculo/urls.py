6c7857f5917088247448f0bf9e6265c2a2081225
RSANTOSERPPLUS <rafael.santos@erpplus.com.br>
2018-10-25 19:59:31 -0300

    Alteração na pagina de contatos

--- curriculo/urls.py
+++ curriculo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from curriculo import views
 
 urlpatterns = [
-    path('<str:sigla>/', views.curso, name="curso")
+    path('<str:sigla>/', views.curso, name="curso"),
+    path('<str:sigladisc>/', views.disciplina, name="disciplina")
 ]
