16bd3e8cba0658050301e35de927a5f99203ef46
Kleber <kleberm@riachuelo.com.br>
2018-10-25 20:16:52 -0300

    Corração URLS

--- curriculo/urls.py
+++ curriculo/urls.py
@@ -4,9 +4,5 @@ from curriculo import views
 
 urlpatterns = [
     path('<str:sigla>/', views.curso, name="curso"),
-<<<<<<< HEAD
-    path('<str:sigladisc>/', views.disciplina, name="disciplina")
-=======
     path('<str:sigla>/disciplina/<str:sigla_disciplina>/', views.disciplina, name="disciplina")
->>>>>>> kleber
 ]
